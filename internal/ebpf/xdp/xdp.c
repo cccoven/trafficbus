bcd2af549c4b36ddea85c370c22d54d63df28d87
cccoven <stayhunger0201@gmail.com>
2023-11-30 11:39:11 +0000

    for each map elem

--- internal/ebpf/xdp/xdp.c
+++ internal/ebpf/xdp/xdp.c
@@ -5,7 +5,7 @@
 
 char __license[] SEC("license") = "Dual MIT/GPL";
 
-#define MAX_RULES 10
+#define MAX_RULES 2
 
 enum protocol {
     TCP = 0,
@@ -33,64 +33,42 @@ const enum protocol *prot __attribute__((unused));
 struct {
 	__uint(type, BPF_MAP_TYPE_ARRAY);
 	__uint(max_entries, MAX_RULES);
-    __type(key, u32);
+    __type(key, __u32);
     __type(value, struct xdp_rule);
 } xdp_rule_map SEC(".maps");
 
-long callback_fn(void *map, const void *key, void *value, void *ctx) {
-    const char info[] = "key: %d\n";
-    bpf_trace_printk(info, sizeof(info), key);
 
+struct callback_ctx {
+    __u32 action;
+};
+
+static __u64 callback_fn(void *map, __u32 *key, struct xdp_rule *value, struct callback_ctx *ctx) {
+    const char debug[] = "idx: %d\n";
+    bpf_trace_printk(debug, sizeof(debug), *key);
+
+    const char info[] = "idx: %d, source: %d,  dst: %d\n";
+    bpf_trace_printk(info, sizeof(info), *key, value->source, value->destination);
+
+    if (*key == 0) {
+        ctx->action = value->target;
+
+        // end loop
+        return 1;
+    }
+    
     return 0;
 }
 
+
 SEC("xdp")
 int xdp_prod_func(struct xdp_md *ctx) {
+    struct callback_ctx cb_ctx = {
+        .action = 0,
+    };
+    bpf_for_each_map_elem(&xdp_rule_map, callback_fn, &cb_ctx, BPF_ANY);
 
-    // u32 k = 0;
-    // struct xdp_rule *rule;
-    // rule = bpf_map_lookup_elem(&xdp_rule_map, &k);
-    // if (rule) {
-    //     const char info[] = " source: %d,  dst: %d\n";
-    //     bpf_trace_printk(info, sizeof(info), rule->source, rule->destination);
-    // }
-    // k = 1;
-    // rule = bpf_map_lookup_elem(&xdp_rule_map, &k);
-    // if (rule) {
-    //     const char info[] = " source: %d,  dst: %d\n";
-    //     bpf_trace_printk(info, sizeof(info), rule->source, rule->destination);
-    // }
+    const char debug[] = "rule result: %d\n";
+    bpf_trace_printk(debug, sizeof(debug), cb_ctx.action);
 
-    u32 i;
-    struct xdp_rule *rule;
-    for (i = 0; i < 1; i++) {
-        const char debug[] = "index: %d\n";
-        bpf_trace_printk(debug, sizeof(debug), i);
-
-        rule = bpf_map_lookup_elem(&xdp_rule_map, &i);
-        if (!rule) {
-            continue;
-        }
-
-        // const char info[] = " source: %d,  dst: %d\n";
-        // bpf_trace_printk(info, sizeof(info), rule->source, rule->destination);
-    }
-
-    // u32 i;
-    // struct xdp_rule *rule;
-    // while (i < MAX_RULES) {
-    //     const char debug[] = "index: %d\n";
-    //     bpf_trace_printk(debug, sizeof(debug), i);
-
-    //     rule = bpf_map_lookup_elem(&xdp_rule_map, &i);
-    //     if (rule) {
-    //         const char info[] = " source: %d,  dst: %d\n";
-    //         bpf_trace_printk(info, sizeof(info), rule->source, rule->destination);
-    //     }
-    //     break;
-    // }
-
-    // bpf_for_each_map_elem(&xdp_rule_map, callback_fn, , BPF_ANY);
-
-    return XDP_PASS;
+    return cb_ctx.action;
 }
